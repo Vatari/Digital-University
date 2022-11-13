ff55e657884e76888264e173484525f0a19ad050
vatari <majestic13@gmail.com>
2022-11-13 19:54:53 +0200

    added events components

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css'],
+})
+export class EventThumbnailComponent {
+  @Input() event: any;
+
+  getStartTimeClass(): any {
+    if (this.event && this.event.time === '8:00 am')
+      return { color: '#003300', 'font-weight': 'bold' };
+    return {};
+  }
+}

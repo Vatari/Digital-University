82d809e8fbd4523d8cade14e71dede9d103c299c
vatari <majestic13@gmail.com>
2022-11-16 11:54:15 +0200

    added profile form

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,13 +1,44 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Component({
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
+  profileForm!: FormGroup;
 
-  ngOnInit(): void {
-      
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    let firstName = new FormControl(
+      this.authService.currentUser.firstName,
+      Validators.required
+    );
+    let lastName = new FormControl(
+      this.authService.currentUser.lastName,
+      Validators.required
+    );
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName,
+    });
   }
 
+  saveProfile(formValues: any) {
+    if (this.profileForm.valid) {
+      this.authService.updateCurrentUser(
+        formValues.firstName,
+        formValues.lastName
+      );
+      this.router.navigate(['events']);
+    }
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
+  }
 }

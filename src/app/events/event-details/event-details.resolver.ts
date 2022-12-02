5ec67bf7ab1de4ff1a3891f3d9912f33c8da896a
vatari <majestic13@gmail.com>
2022-12-02 13:27:08 +0200

    fixes

--- src/app/events/event-details/event-details.resolver.ts
+++ src/app/events/event-details/event-details.resolver.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, Router } from '@angular/router';
-import { EventService, IEvent } from '../shared';
+import { catchError, map } from 'rxjs';
+import { EventService } from '../shared';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,17 @@ export class EventDetailsResolver implements Resolve<any> {
   constructor(private eventService: EventService, private route: Router) {}
 
   resolve(route: ActivatedRouteSnapshot) {
-    return this.eventService.getEvent(route.params['id']);
- 
+    return this.eventService.getEvent(route.params['id']).pipe(
+      map((data) => {
+        return data;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        if (error.status != 200) {
+          this.route.navigate(['/404']);
+        } else {
+          return error.error;
+        }
+      })
+    );
   }
 }

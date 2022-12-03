0032341dbd4a1a03b860e32a04f7ed803f5ffc4a
vatari <majestic13@gmail.com>
2022-12-03 17:11:46 +0200

    code refactoring

--- src/app/events/shared/event-list-resolver.service.ts
+++ src/app/events/shared/event-list-resolver.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Resolve, Router } from '@angular/router';
-import { catchError, map } from 'rxjs';
+import { map } from 'rxjs';
 import { EventService } from './event.service';
 
 @Injectable({
@@ -10,6 +10,5 @@ export class EventListResolverService implements Resolve<any> {
   constructor(private eventService: EventService, private router: Router) {}
   resolve() {
     return this.eventService.getEvents().pipe(map((events) => events));
-
   }
 }
